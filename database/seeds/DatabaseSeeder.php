598024559c8501d8364921a9c27a4a6f93f2e4fc
Anyushu <suzuki@lamp.jp>
2020-12-14 16:52:50 +0900

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DefaultGenreSeeder::class,
             ManagesTableSeeder::class,
-            ProductSeeder::class,
         ]);
     }
 }
